7fa0b4ea3424cf17f8e61d3455788b71e3c7f48f
ugyenchhedaugyenchhe <ugyenchheda@gmail.com>
2023-08-22 23:35:17 +0300

    added few question

--- quizgame/quiz_game.py
+++ quizgame/quiz_game.py
@@ -1,3 +1,32 @@
 print("Welcome to my computer quiz")
 
 playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print('Correct!')
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer == "random access unit":
+    print('Correct!')
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print('Correct!')
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer == "power supply":
+    print('Correct!')
+else:
+    print("Incorrect!")
