4b8b643c097d06e193784efb11ef665b7a2563fd
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-08-30 21:16:09 +0300

    Update and rename simplecalculator to simplecalculator.py
    added code for finding reminder

--- simplecalculator.py
+++ simplecalculator.py
@@ -10,35 +10,44 @@ def mul(a, b):
 def div(a, b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer) + "\n")
+def rem(a, b):
+    answer = a % b
+    print(str(a) + " % " + str(b) + " = " + str(answer) + "\n")
 
 while True:
     print("A. Addition")
     print("B. Subtraction")
     print("C. Multiplication")
     print("D. Division")
-    print("E. Exit")
-    choice = input("Input your choice: ")
+    print("E. Reminder")
+    print("F. Exit")
+    choice = input("Enter your choice: ")
 
     if choice == "a" or choice == "A":
         print("Addition")
-        a = int(Input("Input first number: "))
-        b = int(input("input second number: "))
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         add(a, b)
     elif choice == "b" or choice == "B":
         print("Subtraction")
-        a = int(input("Input first number:"))
-        b = int(input("Input second number: "))
+        a = int(input("Enter first number:"))
+        b = int(input("Enter second number: "))
         sub(a, b)
     elif choice == "c" or choice == "C":
         print("Multiplication")
-        a = int(input("Input first number:"))
-        b = int(input("Input second number: "))
+        a = int(input("Enter first number:"))
+        b = int(input("Enter second number: "))
         mul(a, b)
     elif choice == "d" or choice == "D":
         print("Division" )
-        a = int(input("Input first number:"))
-        b = int(input("Input second number: "))
+        a = int(input("Enter first number:"))
+        b = int(input("Enter second number: "))
         div(a, b)
     elif choice == "e" or choice == "E":
-        print("program ended")
+        print("Reminder" )
+        a = int(input("Enter first number:"))
+        b = int(input("Enter second number: "))
+        rem(a, b)
+    elif choice == "f" or choice == "F":
+        print("Completed!")
         quit()
