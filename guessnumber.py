9ccf8911c0f3b638f5c6c3b68074b6ffbfa79a95
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-08-31 22:52:13 +0300

    Update guessnumber.py
    function created to run computer guess

--- guessnumber.py
+++ guessnumber.py
@@ -12,3 +12,23 @@ def guess(x):
 
     print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low  # could also be high b/c low = high
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+
+    print(f'Yay! The computer guessed your number, {guess}, correctly!')
+
+
+guess(10)
+
