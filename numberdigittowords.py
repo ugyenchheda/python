6ab6d0b7ea1106ebb9ce77b4bfd1526aefd6e1c8
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-01 20:06:22 +0300

    Update numberdigittowords.py
    wrapup with converting the digit to words.

--- numberdigittowords.py
+++ numberdigittowords.py
@@ -22,3 +22,17 @@ pos_format_label = Label(root, text="1. Positives :  ", fg="green", font=("varde
 neg_format_label = Label(root, text="2. Negatives ", fg="green", font=("vardena", 10, 'bold')).place(x=50, y=110)
 float_format_label = Label(root, text="3. Zeros  ", fg="green", font=("vardena", 10, 'bold')).place(x=50, y=130)
 zero_format_label = Label(root, text="4. Floating points/decimals/fractions  ", fg="green", font=("vardena", 10, 'bold')).place(x=50, y=150)
+
+btn = Button(master=root, text="calculate",fg="white",width=20, font=("vardena", 10, 'bold') ,command=num_to_words, bg="green").place(x=100,y=230)
+
+display = Label(root, text="",fg="black", font=("vardena", 10, 'bold'))
+display.place(x=10, y=300)
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+
+#give image location here
+image_path = os.path.join(script_directory, 'digittospell', 'number.png')
+photo = PhotoImage(file=image_path)
+root.iconphoto(False, photo)
+
+root.mainloop()
