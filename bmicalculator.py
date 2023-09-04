a797594b2bfe004194c04836e5e599891fd3e7fe
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-04 23:35:16 +0300

    Update bmicalculator.py
    display the difference in BMI units when the user falls under the "underweight," "overweight," or "severely overweight" categories.

--- bmicalculator.py
+++ bmicalculator.py
@@ -6,14 +6,20 @@ BMI = Weight / (Height * Height)
 
 if BMI > 0:
     if BMI <= 16:
-        print(f"{user_name}, you are severely underweight")
+        print(f"{user_name}, you are severely underweight.")
     elif BMI <= 18.5:
-        print(f"{user_name}, you are underweight")
+        healthy_bmi_upper = 18.5
+        bmi_difference = healthy_bmi_upper - BMI
+        print(f"{user_name}, you are underweight by {bmi_difference:.2f} BMI units.")
     elif BMI <= 25:
-        print(f"{user_name}, you are healthy")
+        print(f"{user_name}, you are within a healthy BMI range.")
     elif BMI <= 30:
-        print(f"{user_name}, you are overweight")
+        healthy_bmi_upper = 25
+        bmi_difference = BMI - healthy_bmi_upper
+        print(f"{user_name}, you are overweight by {bmi_difference:.2f} BMI units.")
     else:
-        print(f"{user_name}, you are severely overweight")
+        healthy_bmi_upper = 30
+        bmi_difference = BMI - healthy_bmi_upper
+        print(f"{user_name}, you are severely overweight by {bmi_difference:.2f} BMI units.")
 else:
     print("Enter valid details")
