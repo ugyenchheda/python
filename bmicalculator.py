32b8e916e871a6a19b3962398f38f702d7f62618
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-04 23:21:52 +0300

    Update bmicalculator.py
    used  f-string to show output in formatted way.

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,20 +1,19 @@
-user_name =input("Please enter your name: ")
+user_name = input("Please enter your name: ")
 Height = float(input("Enter your height in centimeters: "))
 Weight = float(input("Enter your weight in Kg: "))
 Height = Height / 100
 BMI = Weight / (Height * Height)
-print("Your Body Mass Index is: ", BMI)
 
 if BMI > 0:
     if BMI <= 16:
-        print(user_name, ", you are severely underweight")
+        print(f"{user_name}, you are severely underweight")
     elif BMI <= 18.5:
-        print(user_name, ", you are underweight")
+        print(f"{user_name}, you are underweight")
     elif BMI <= 25:
-        print(user_name, ", you are healthy")
+        print(f"{user_name}, you are healthy")
     elif BMI <= 30:
-        print(user_name, ", you are overweight")
+        print(f"{user_name}, you are overweight")
     else:
-        print(user_name, ", you are severely overweight")
+        print(f"{user_name}, you are severely overweight")
 else:
     print("Enter valid details")
