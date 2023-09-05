cebd5e22e45bf7743543bb1b83cbe0d6f4474c3c
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-06 00:59:37 +0300

    Update missingnumber.py
    optimized code removing for loop and creating set

--- missingnumber.py
+++ missingnumber.py
@@ -8,3 +8,15 @@ def missing_number(n):
     return output
 all_numbers = [1, 3,4, 5, 6, 7, 9, 10, 11, 14, 17, 18, 20]
 print(missing_number(all_numbers))
+
+#optimized way
+
+def missing_numbers(nums):
+    nums_set = set(nums)
+    min_num, max_num = min(nums), max(nums)
+    all_numbers = set(range(min_num, max_num + 1))
+    missing = list(all_numbers - nums_set)
+    return missing
+
+all_numbers = [1, 3, 4, 5, 6, 7, 9, 10, 11, 14, 17, 18, 20]
+print(missing_numbers(all_numbers))
