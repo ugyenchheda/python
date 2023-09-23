dddba792cda50189ba2da66398d1bcd47c1bf9dc
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-09-23 22:56:38 +0300

    Update ugyengame.py
    added function to move left and to merge

--- ugyengame.py
+++ ugyengame.py
@@ -8,7 +8,8 @@ class Game(Frame):
         self.master.title("Ugyen")
         self.main_grid = Frame(self, bg='Black', bd=3, width=500,height=500)
         self.main_grid.grid()
-      
+ 
+#crete board     
 def gameboard(self):
         self.tiles = []
         for i in range(4):
@@ -36,3 +37,22 @@ def gameboard(self):
                 }
                 row.append(tile_data)
             self.tiles.append(row)
+            
+#function to move left
+def moveToLeft(self):
+        new_board = [[0 for col in range(4)] for row in range(4)]
+        for i in range(4):
+            fill_position = 0
+            for j in range(4):
+                if self.board[i][j] != 0:
+                    new_board[i][fill_position] = self.board[i][j]
+                    fill_position += 1
+        self.board = new_board
+
+#merge function
+    def merge(self):
+        for i in range(4):
+            for j in range(3):
+                if self.board[i][j] != 0 and self.board[i][j] == self.board[i][j+1]:
+                    self.board[i][j] *= 2
+                    self.board[i][j+1] = 0
