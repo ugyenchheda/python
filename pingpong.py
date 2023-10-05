d850f1afc5b4851f4f68dcd5506969ab467764e8
Ugyen C. Lama <86464524+ugyenchheda@users.noreply.github.com>
2023-10-05 22:25:43 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -18,3 +18,57 @@ def move(player, change):
     """Move player position by change."""
     state[player] += change
 
+
+def rectangle(x, y, width, height):
+    """Draw rectangle at (x, y) with given width and height."""
+    up()
+    goto(x, y)
+    down()
+    begin_fill()
+    for count in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+    end_fill()
+
+
+def draw():
+    """Draw game and move pong ball."""
+    clear()
+    rectangle(-200, state[1], 10, 50)
+    rectangle(190, state[2], 10, 50)
+
+    ball.move(aim)
+    x = ball.x
+    y = ball.y
+
+    up()
+    goto(x, y)
+    dot(10)
+    update()
+
+    if y < -200 or y > 200:
+        aim.y = -aim.y
+
+    if x < -185:
+        low = state[1]
+        high = state[1] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    if x > 185:
+        low = state[2]
+        high = state[2] + 50
+
+        if low <= y <= high:
+            aim.x = -aim.x
+        else:
+            return
+
+    ontimer(draw, 50)
+
+
